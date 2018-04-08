142befc7fe519222cac1328720bbb17208b3bf48
Matt Moriarity <matt@mattmoriarity.com>
2018-04-07 20:52:35 -0500

    Add file for post fixtures

---
+++ spec/fixtures/post.rb
@@ -0,0 +1,15 @@
+EXAMPLE_NORMAL_POST = {
+  "post_id" => 123,
+  "post_title" => "This is a post",
+  "post_content" => "This is my #content",
+  "post_type" => "post",
+  "post_format" => "standard",
+}
+
+EXAMPLE_STATUS_POST = {
+  "post_id" => 124,
+  "post_title" => "",
+  "post_content" => "This is a status post.",
+  "post_type" => "post",
+  "post_format" => "status",
+}
--- spec/wordpress/post_spec.rb
+++ spec/wordpress/post_spec.rb
@@ -1,23 +1,14 @@
 require 'spec_helper'
+require 'fixtures/post'
 
 RSpec.describe Wordpress::Post do
-  let(:fields) {
-    {
-      "post_id" => 123,
-      "post_title" => "This is a post",
-      "post_content" => "This is my #content",
-      "post_type" => "post",
-      "post_format" => "status",
-    }
-  }
-
   it "roundtrips from wordpress fields" do
-    post = Wordpress::Post.new(fields)
+    post = Wordpress::Post.new(EXAMPLE_NORMAL_POST)
     expect(post.id).to eq 123
     expect(post.title).to eq "This is a post"
     expect(post.content).to eq "This is my #content"
     expect(post.type).to eq "post"
-    expect(post.format).to eq "status"
-    expect(post.fields).to eq fields
+    expect(post.format).to eq "standard"
+    expect(post.fields).to eq EXAMPLE_NORMAL_POST
   end
 end
