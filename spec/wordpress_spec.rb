a6ae9efa387d31db9afdf5e01dd396b18c924b81
Matt Moriarity <matt@mattmoriarity.com>
2018-04-07 20:52:57 -0500

    Delete dumb default test

--- spec/wordpress_spec.rb
+++ spec/wordpress_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Wordpress do
   it "has a version number" do
     expect(Wordpress::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
