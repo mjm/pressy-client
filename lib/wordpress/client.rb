476f2b0757765a8485e67a9c8e203a82fa6156e7
Matt Moriarity <matt@mattmoriarity.com>
2018-04-15 19:12:41 -0500

    Add fetch_posts for paging posts
    `Wordpress#fetch_posts` returns a collection that implements Enumerable
    and pulls down posts in batches of 20 at a time.

--- lib/wordpress/client.rb
+++ lib/wordpress/client.rb
@@ -1,6 +1,8 @@
 require 'xmlrpc/client'
 
 class Wordpress
+  attr_reader :client, :username, :password
+
   def initialize(client, username, password)
     @client = client
     @username = username
@@ -28,7 +30,7 @@ class Wordpress
     [params, username, password]
   end
 
-  def posts
+  def recent_posts
     @client.call(
       "wp.getPosts",
       1,
@@ -38,4 +40,39 @@ class Wordpress
       Wordpress::Post::WP_FIELDS
    ).map {|params| Wordpress::Post.new(params) }
   end
+
+  def fetch_posts
+    FetchPostsCollection.new(self)
+  end
+
+  class FetchPostsCollection
+    include Enumerable
+
+    def initialize(wp)
+      @wp = wp
+    end
+
+    def each
+      offset = 0
+      posts = fetch_posts(offset)
+
+      until posts.empty?
+        posts.each {|post| yield Wordpress::Post.new(post) }
+        offset += 20
+        posts = fetch_posts(offset)
+      end
+    end
+
+    def fetch_posts(offset=0)
+      @wp.client.call(
+        "wp.getPosts",
+        1,
+        @wp.username,
+        @wp.password,
+        { post_type: "post", offset: offset, number: 20 },
+        Wordpress::Post::WP_FIELDS
+      )
+    end
+  end
+
 end
--- spec/wordpress/client_spec.rb
+++ spec/wordpress/client_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Wordpress do
   let(:username) { "user" }
   let(:password) { "pass" }
   let(:wordpress) { Wordpress.new(client, username, password) }
+  let(:fields) { Wordpress::Post::WP_FIELDS }
 
   context "connecting" do
     it "can be created with a client, username, and password" do
@@ -36,11 +37,40 @@ RSpec.describe Wordpress do
   end
 
   it "can fetch all of the recent posts for main blog" do
-    expect(client).to receive(:call).with("wp.getPosts", 1, username, password, { post_type: "post" }, Wordpress::Post::WP_FIELDS) { [EXAMPLE_NORMAL_POST, EXAMPLE_STATUS_POST] }
-    posts = wordpress.posts
+    expect(client).to receive(:call).with("wp.getPosts", 1, username, password, { post_type: "post" }, fields) { [EXAMPLE_NORMAL_POST, EXAMPLE_STATUS_POST] }
+    posts = wordpress.recent_posts
     expect(posts[0].id).to eq 123
     expect(posts[0].title).to eq "This is a post"
     expect(posts[1].id).to eq 124
     expect(posts[1].format).to eq "status"
+  end
+
+  context "fetching all posts" do
+    context "when there are no posts" do
+      it "fetches the posts with one call" do
+        expect(client).to receive(:call).with("wp.getPosts", 1, username, password, { post_type: "post", number: 20, offset: 0 }, fields) { [] }
+        expect(wordpress.fetch_posts.to_a).to be_empty
+      end
+    end
+
+    context "when there are less than 20 posts" do
+      it "fetches the posts with two calls" do
+        expect(client).to receive(:call).with("wp.getPosts", 1, username, password, { post_type: "post", number: 20, offset: 0 }, fields) { [EXAMPLE_NORMAL_POST] * 15 }
+        expect(client).to receive(:call).with("wp.getPosts", 1, username, password, { post_type: "post", number: 20, offset: 20 }, fields) { [] }
+        posts = wordpress.fetch_posts.to_a
+        expect(posts.count).to eq 15
+        expect(posts.first).to be_a Wordpress::Post
+      end
+    end
+
+    context "when there are more than 20 posts" do
+      it "fetches the posts with several calls" do
+        expect(client).to receive(:call).with("wp.getPosts", 1, username, password, { post_type: "post", number: 20, offset: 0 }, fields) { [EXAMPLE_NORMAL_POST] * 15 }
+        expect(client).to receive(:call).with("wp.getPosts", 1, username, password, { post_type: "post", number: 20, offset: 20 }, fields) { [EXAMPLE_STATUS_POST] * 20 }
+        expect(client).to receive(:call).with("wp.getPosts", 1, username, password, { post_type: "post", number: 20, offset: 40 }, fields) { [] }
+        posts = wordpress.fetch_posts.to_a
+        expect(posts.count).to eq 35
+      end
+    end
   end
 end
