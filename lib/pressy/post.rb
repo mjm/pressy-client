487cbb379cb4c658993625d36c8773d3eab145e8
Matt Moriarity <matt@mattmoriarity.com>
2018-05-16 12:17:36 -0500

    Add documentation for Post

--- lib/pressy/post.rb
+++ lib/pressy/post.rb
@@ -1,4 +1,8 @@
+# A Post represents a single blog post in WordPress.
+# It serves as a value object for sending and receive information about blog
+# posts using a {Pressy::Client}.
 class Pressy::Post
+  # The default set of fields to request from the WordPress API.
   WP_FIELDS = %w{
     post_id
     post_title
@@ -10,8 +14,55 @@ class Pressy::Post
     post_modified_gmt
   }
 
-  attr_accessor :id, :title, :content, :type, :status, :format, :published_at, :modified_at
+  # The ID of the post.
+  # This maps to the +"post_id"+ field in the WordPress API.
+  # @return [Fixnum] The ID of the post, or nil if the post is not yet saved.
+  attr_accessor :id
+  # The title of the post.
+  # This maps to the +"post_title"+ field in the WordPress API.
+  # @return [String] The title of the post, or +""+ if the post is untitled.
+  attr_accessor :title
+  # The body of the post.
+  # This maps to the +"post_content"+ field in the WordPress API.
+  # @return [String] The body of the post.
+  attr_accessor :content
+  # The type of "post" this represents.
+  #
+  # Right now, this always has the value +"post"+ because we don't support API
+  # that interacts with pages.
+  #
+  # This maps to the +"post_type"+ field in the WordPress API.
+  # @return [String] The type of the post, or +"post"+ as a default.
+  attr_accessor :type
+  # The publication status of the post. Posts start with a status of +"draft"+.
+  #
+  # This maps to the +"post_status"+ field in the WordPress API.
+  # @return [String] The status of the post, or +"draft"+ as a default.
+  attr_accessor :status
+  # The format of the post. Not all themes use formats, but some use it to
+  # indicate the sort of content that that post contains and present the post
+  # differently.
+  #
+  # This maps to the +"post_format"+ field in the WordPress API.
+  # @return [String] The format of the post.
+  attr_accessor :format
+  # The timestamp when the post was published, or when it is set to be
+  # published.
+  #
+  # This maps to the +"post_date_gmt"+ field in the WordPress API.
+  # @return [Time] The time when the post was or will be published.
+  attr_accessor :published_at
+  # The timestamp when the post was last modified.
+  # This maps to the +"post_modified_gmt"+ field in the WordPress API.
+  # @return [Time] The time when the post was modified.
+  attr_accessor :modified_at
 
+  # Create a new post with the given field values.
+  #
+  # The parameters are expected to use the same keys and types as would be
+  # expected to come from the WordPress API.
+  #
+  # @param params [Hash<String, Object>] The field values of the post.
   def initialize(params)
     @id = params["post_id"]&.to_i
     @title = params.fetch("post_title", "")
@@ -27,6 +78,8 @@ class Pressy::Post
     end
   end
 
+  # Create a [Hash] of the post data for use by the WordPress API.
+  # @return [Hash<String, Object>] A hash of post data.
   def fields
     fields = {
       "post_title" => title,
@@ -41,11 +94,16 @@ class Pressy::Post
     fields
   end
 
+  # Create a new post based on this one, with some fields changed.
+  # @param params [Hash<String, Object>] The field values to change.
+  # @return [Pressy::Post] a new post with the updated values.
   def with(params)
     new_params = fields.merge(params)
     self.class.new(new_params)
   end
 
+  # Compare two posts based on the contents of their fields.
+  # @return [Boolean] true if the posts have all the same attributes, false otherwise.
   def ==(other)
     id == other.id &&
       title == other.title &&
